18f033d0896adb51c08277f50410eaa2781828d5
Damien Oriente <damiodev@icloud.com>
2024-11-05 23:40:11 +0100

    Installation of Materialize

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+require('./materialize.js');
+
+document.addEventListener('DOMContentLoaded', function () {
+  M.AutoInit();
+
+  var elems = document.querySelectorAll('.tooltipped');
+  M.Tooltip.init(elems);
+});
